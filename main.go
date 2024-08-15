013531986b5a33c15bc397a9d69435f43dde3c08
eggnocent <egiwiratamaaa@gmail.com>
2024-08-15 11:45:59 +0700

    nambah reoute

--- main.go
+++ main.go
@@ -9,11 +9,40 @@ import (
 func main() {
 	router := gin.Default()
 
-	router.GET("/", func(ctx *gin.Context) {
-		ctx.JSON(http.StatusOK, gin.H{
-			"nama": "M Iksan",
-			"bio":  "pemain sepakbola asal haiti",
-		})
-	})
+	router.GET("/", rootHandler)
+	router.GET("/fact", factHandler)
+	router.GET("/buku/:id/:title", bukuHandler)
+	router.GET("/query", queryHandler)
 	router.Run()
+}
+
+func rootHandler(ctx *gin.Context) {
+	ctx.JSON(http.StatusOK, gin.H{
+		"nama": "M Iksan",
+		"bio":  "pemain sepakbola asal haiti",
+	})
+}
+
+func factHandler(ctx *gin.Context) {
+	ctx.JSON(http.StatusOK, gin.H{
+		"funfact":         "Bisa melakukan dribbling dengan mata kaki",
+		"makanan favorit": "tengkleng pak bayu",
+	})
+}
+
+func bukuHandler(ctx *gin.Context) {
+	id := ctx.Param("id")
+	title := ctx.Param("title")
+	ctx.JSON(http.StatusOK, gin.H{
+		"id":    id,
+		"title": title,
+	})
+}
+
+func queryHandler(ctx *gin.Context) {
+	title := ctx.Query("title")
+
+	ctx.JSON(http.StatusOK, gin.H{
+		"title": title,
+	})
 }
