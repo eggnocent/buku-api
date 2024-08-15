21e2024e299dd16b000f51aaf64d76654f63ff92
eggnocent <egiwiratamaaa@gmail.com>
2024-08-15 15:18:02 +0700

    mengambil data

--- main.go
+++ main.go
@@ -22,19 +22,45 @@ func main() {
 	db.AutoMigrate(&book.Buku{})
 	fmt.Println("Database berhasil terkoneksi")
 
-	buku := book.Buku{}
-	buku.Judul = "kucing langit"
-	buku.Harga = 100000
-	buku.Diskon = 10
-	buku.Rating = 5
-	buku.Deskripsi = "baguy"
+	// CREATE
+	// buku := book.Buku{}
+	// buku.Judul = "kipas angin kesedot sampah"
+	// buku.Harga = 250000
+	// buku.Diskon = 11
+	// buku.Rating = 5
+	// buku.Deskripsi = "baguy"
 
-	err = db.Create((&buku)).Error
+	// err = db.Create((&buku)).Error
+	// if err != nil {
+	// 	fmt.Println("========================")
+	// 	fmt.Println("gagal menambahkan data")
+	// 	fmt.Println("========================")
+	// }
+
+	// READ
+	// var buku book.Buku
+	// err = db.First(&buku).Error
+	// if err != nil {
+	// 	fmt.Println("========================")
+	// 	fmt.Println("gagal mengambil data")
+	// 	fmt.Println("========================")
+	// }
+
+	// fmt.Println("Judul: ", buku.Judul)
+	// fmt.Printf("objek buku %v", buku)
+
+	// READ LAST
+	var buku book.Buku
+	err = db.Last(&buku).Error
 	if err != nil {
 		fmt.Println("========================")
-		fmt.Println("sukses menambahkan data")
+		fmt.Println("gagal mengambil data:", err) // Cetak error detail
 		fmt.Println("========================")
+		return
 	}
+
+	fmt.Println("Judul: ", buku.Judul)
+	fmt.Printf("objek buku: %+v\n", buku)
 
 	router := gin.Default()
 
